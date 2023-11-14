f1af880d2b70e73cc1d126b68f29f1cb3388fc8b
Donphili <donphilip65@yahoo.com>
2023-11-14 22:02:20 +0100

    add

--- calculator.js
+++ calculator.js
@@ -7,3 +7,11 @@ function appendToDisplay(value) {
 function clearDisplay() {
     display.value = '';
 }
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
